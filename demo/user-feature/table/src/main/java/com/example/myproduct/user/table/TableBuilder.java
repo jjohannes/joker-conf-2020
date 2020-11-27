48353aebeb1f3584591e86efe6dca9c573734944
Jendrik Johannes <jendrik@gradle.com>
2020-11-27 22:57:15 +0100

    Slight sample code improvements

--- demo/user-feature/table/src/main/java/com/example/myproduct/user/table/TableBuilder.java
+++ demo/user-feature/table/src/main/java/com/example/myproduct/user/table/TableBuilder.java
@@ -3,20 +3,20 @@ package com.example.myproduct.user.table;
 import com.example.myproduct.model.MyProductRelease;
 import com.example.myproduct.user.data.DataRetriever;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class TableBuilder {
 
-    public static List<List<String>> build() {
+    public static Map<String, String> build() {
         List<MyProductRelease> releases = DataRetriever.retrieve().getReleases();
         return build(releases);
     }
 
-    protected static List<List<String>> build(List<MyProductRelease> releases) {
-        return releases.stream().map(r ->
-                Arrays.asList(r.getVersion(), r.getReleaseNotes())).collect(Collectors.toList());
+    protected static Map<String, String> build(List<MyProductRelease> releases) {
+        return releases.stream().collect(Collectors.toMap(
+                MyProductRelease::getVersion, MyProductRelease::getReleaseNotes));
     }
 
 }
